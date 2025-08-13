105310f0962dae2ea43a079dba235fb6968f0e7f
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-08-14 01:24:48 +0200

    Add views and urls

---
+++ app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from app import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'tags', views.TagViewSet)
+router.register(r'projects', views.ProjectViewSet)
+router.register(r'comments', views.CommentViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import Tag, Project, Comment
+from rest_framework import permissions, viewsets
+from .serializers import UserSerializer, TagSerializer, ProjectSerializer, CommentSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
