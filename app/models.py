944868642d1e325fb0824c486f64d5db72e3fc71
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-08-14 00:25:29 +0200

    Add Models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=16)
+
+
+class Project(models.Model):
+    SHIRT_SIZES = {
+        "S": "Small",
+        "M": "Medium",
+        "L": "Large",
+    }
+
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+    tags = models.ManyToManyField(Tag, related_name="projects")
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    timestamp = models.DateTimeField()
+    repository_link = models.URLField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
