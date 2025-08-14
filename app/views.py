7398c2966ffbce76098d1c1924f7a1b08bfd13bb
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-08-14 02:03:10 +0200

    Add WIP view modification for comment and project

--- app/views.py
+++ app/views.py
@@ -27,8 +27,36 @@ class ProjectViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
+    '''
+        TODO
+        overwrite create methods so that the user is set automatically or we can bypass this issue
+        user = serializers.StringRelatedField()
+        currently when we specify this in serializers.py, retrieving the model means we get a user string instead of id,
+        but when trying to create a new instance of the object we cannot specify the user and he remains null
+    '''
+
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    '''
+        TODO
+        overwrite create methods so that the user is set automatically or we can bypass this issue
+        user = serializers.StringRelatedField()
+        currently when we specify this in serializers.py, retrieving the model means we get a user string instead of id,
+        but when trying to create a new instance of the object we cannot specify the user and he remains null
+    '''
+
+    def destroy(self, request, *args, **kwargs):
+        # but now we're gonna do some additional checks BEFORE that
+
+        # TODO:
+        # 1. we need to know what user sent the request
+        # 2. we need to know if the user is an owner of all the comments this request refers to
+        # 3. if NOT then we return 401 early
+        print("WE ARE DELETING, ARENT WE??")
+
+        # this should not change the behaviour
+        super().destroy(request, *args, **kwargs)
