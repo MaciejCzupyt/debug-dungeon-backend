eaf284d58cbcf557fb2505ae63e21312fc198f72
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-08-16 17:13:47 +0200

    Update CommentSerializer to automatically assign user field to authenticated user

--- app/serializers.py
+++ app/serializers.py
@@ -40,8 +40,15 @@ class ProjectSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    # user = serializers.StringRelatedField()
+    user = serializers.StringRelatedField()
 
     class Meta:
         model = Comment
         fields = "__all__"
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        if request and hasattr(request, 'user'):
+            validated_data['user'] = request.user
+        return super().create(validated_data)
+
