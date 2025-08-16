506728a4711c2016c53126fdb7ee176ce72210e6
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-08-16 15:01:46 +0200

    Change Tag model to use Name as primary key instead of Id

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=16, unique=True)
+    name = models.CharField(max_length=16, unique=True, primary_key=True)
 
     def __str__(self):
         return self.name
@@ -36,5 +36,7 @@ class Comment(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
+    # def __str__(self):
+    #     return self.content[:16]
     def __str__(self):
-        return self.content[:16]
+        return f"{self.user.username},{self.project.title},{self.created}"
