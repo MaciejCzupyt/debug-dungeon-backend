9eee7110dfc23481f22e990e53df79fffb96996b
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-09-01 20:14:19 +0200

    Add IsSelfOrReadOnly permission for managing user resources

--- backend/permissions.py
+++ backend/permissions.py
@@ -12,3 +12,15 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             return True
 
         return obj.user == request.user
+
+
+class IsSelfOrReadOnly(permissions.BasePermission):
+    """
+    Only allow users to edit themselves.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj == request.user
